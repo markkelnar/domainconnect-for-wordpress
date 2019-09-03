23c395d2d6e9b5ab15d86e476bd09726a8cc0316
Mark Kelnar <markkelnar@gmail.com>
2019-09-03 17:41:29 -0500

    Add domain connect plugin

---
+++ domainconnect-shortcode/domainconnect-shortcode.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Domain Connect Shortcode
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     A way to expose easy domain setup links for domains that support domain connect
+ * Author:          Mark Kel;nar
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     domainconnect-shortcode
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Domainconnect_Shortcode
+ */
+
+// Your code starts here.
